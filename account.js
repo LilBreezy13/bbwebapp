ddf41e46fb942e6a32f2b5922eda551cd24f5a74
G shine <nickyjay013@gmail.com>
2024-05-20 22:08:47 +0000

    sign in

--- account.js
+++ account.js
@@ -1,8 +1,28 @@
-const switchers = [...document.querySelectorAll('.switcher')]
 
-switchers.forEach(item => {
-	item.addEventListener('click', function() {
-		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-		this.parentElement.classList.add('is-active')
-	})
-})
+
+//form submit
+
+
+let email = document.querySelector('.email');
+let psd = document.querySelector('.password');
+let check = document.querySelector('#rem');
+let msg = document.querySelectorAll('.msg');
+
+
+const refresh = () =>{
+  
+  if(email.value == ""){
+    msg[0].innerText = "Please enter a valid email address or phone number.";
+  }if(psd.value == ""){
+    msg[1].innerText = "Your password must contain between 4 and 60 characters.";
+  }else{
+        msg[0].innerText = "";
+        msg[1].innerText = "";
+        alert('Sign-in succesfully');
+  }
+  
+  email.value = "";
+  psd.value = "";
+  check.checked = "";
+ 
+}
