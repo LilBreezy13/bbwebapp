a439b206031d1388a74a0b0bca7e43d2168c5695
G shine <nickyjay013@gmail.com>
2024-05-21 16:45:15 +0000

    save

--- registration.js
+++ registration.js
@@ -19,3 +19,47 @@ options.forEach((option) => {
     select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
   });
 });
+
+
+
+
+
+/////////////////////////////////////////////////
+
+function showForm() {
+  // Hide all forms
+  document.getElementById('studentForm').style.display = 'none';
+  document.getElementById('teacherForm').style.display = 'none';
+  document.getElementById('adminForm').style.display = 'none';
+
+  // Show the selected form
+  var registrationType = document.getElementById('registrationType').value;
+  if (registrationType === 'student') {
+      document.getElementById('studentForm').style.display = 'block';
+  } else if (registrationType === 'teacher') {
+      document.getElementById('teacherForm').style.display = 'block';
+  } else if (registrationType === 'admin') {
+      document.getElementById('adminForm').style.display = 'block';
+  }
+}
+
+
+/////////////////////////////////////////
+
+// function decrement() {
+//   var input = document.getElementById('counterInput');
+//   var value = parseInt(input.value, 10);
+//   if (value > parseInt(input.min, 10)) {
+//       input.value = value - 1;
+//   }
+// }
+
+// function increment() {
+//   var input = document.getElementById('counterInput');
+//   var value = parseInt(input.value, 10);
+//   if (value < parseInt(input.max, 10)) {
+//       input.value = value + 1;
+//   }
+// }
+
+
